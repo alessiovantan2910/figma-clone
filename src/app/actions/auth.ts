0060de10461daed332821cf996dd4cbe467e4688
kogachi <kogachi872.kdgo.student@gmail.com>
2025-11-08 12:25:24 +0900

    11/8

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -1,6 +1,10 @@
 "use server";
 
+import { ZodError } from "zod";
 import { signUpSchema } from "~/schema";
+import {db} from '~/server/db'
+import bcrypt from "bcryptjs";
+import { redirect } from "next/dist/server/api-utils";
 
 export async function register(
     prevSate: string | undefined, 
@@ -8,11 +12,33 @@ export async function register(
         try{
             const{email, password} = 
          await signUpSchema.parseAsync({
-            email: formData.get("email") as String,
+            email: formData.get("email"),
             password: formData.get("password")
          });  
+
+         const user  = await db.user.findUnique({
+            where:{
+                email: email
+            }
+         })
+
+         if(!user){
+            return "ユーザーが既に登録してます。"
+         }
+
+         const hash = await bcrypt.hash(password, 8)
+
+         await db.user.create({
+            data: {
+                email: email,
+                password: hash,
+            }
+         })
+
         }catch(error){
-                return "error"
+                if (error instanceof ZodError){
+                    return error.errors.map((error) => error.message).join(",");
+                }
         }
-       
+       //redirect("/signin") 11/12に修正する
     }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,3 +1,45 @@
+"use client";
+
+import { register } from "../actions/auth";
+import Link from "next/link"
+import { useActionState } from "react";
+
 export default function Page(){
-    return <p>sign up</p>
+    const [errorMessage, fromAction, isPending] = useActionState(
+       register,
+       undefined,
+ );
+    return <div className="flex min-h-screen items-center justify-center bg-white px-4  ">
+        <div className="w-full max-w-sm space-y-6 ">
+            <h1 className="text-center text-2xl font-bold text-gray-600">
+                SignUp
+                </h1>
+                <form action={fromAction} className="space-y-4">
+                    <div className="relative h-fit">
+                    <input className="w-full rounded-md border border-gray-300 text-sm px-3 pt-7 focus:border-black focus:outline-none" 
+                    type="email" 
+                    name="email"
+                     required
+                     />
+                    <label className="absolute left-3 top-2 text-[12px]">Email</label>
+                    </div>
+                    <div className="relative h-fit">
+                    <input className="w-full rounded-md border border-gray-300 text-sm px-3 pt-7 focus:border-black focus:outline-none" 
+                    type="password" 
+                    name="password"
+                    minLength={8}
+                     required
+                     />
+                    <label className="absolute left-3 top-2 text-[12px]">Password</label>
+                    </div>
+                    <button className="w-full rounded-md  bg-black text-white py-2 text-sm font-bold hover:bg-gray-600 focus:outline-none disabled:cursor-not-allowed  disabled:bg-gray-300">
+                        Register
+                        </button>
+                        <p className="text-center text-xs text-gray-500 ">アカウントは既にありますか？<Link  className="text-blue-400 hover:text-blue-600" href="/signin">
+                        ログイン</Link>
+                        </p>
+                        {errorMessage && (<p className="text-center text-sm text-red-500">{errorMessage}</p>)}
+                </form>
+        </div>
+    </div>
 }
